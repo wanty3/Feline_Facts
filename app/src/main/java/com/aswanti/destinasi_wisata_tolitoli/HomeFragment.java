980b1552247aa4fb9e1e22e036ef90b1e2645e69
wanty33 <wanty047@gmail.com>
2024-11-23 19:40:20 +0800

    fixing fragment home

--- app/src/main/java/com/aswanti/destinasi_wisata_tolitoli/HomeFragment.java
+++ app/src/main/java/com/aswanti/destinasi_wisata_tolitoli/HomeFragment.java
@@ -1,6 +1,23 @@
 package com.aswanti.destinasi_wisata_tolitoli;
 
-import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class HomeFragment extends Activity {
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+    }
 }
